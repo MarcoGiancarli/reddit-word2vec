9ff9582a8d1c7cc25e73429861ba6ae00cfd9085
MarcoGiancarli <m.a.giancarli@gmail.com>
2015-08-03 15:39:50 -0400

    changed formatting so that everything is lowercase with no punctuation

--- src/train.py
+++ src/train.py
@@ -1,9 +1,12 @@
 __author__ = 'Marco Giancarli -- m.a.giancarli@gmail.com'
 
 
-from nltk import tokenize
-from gensim.models import word2vec
 import os
+import re
+from nltk.tokenize import sent_tokenize
+
+from gensim.models import phrases
+from gensim.models import word2vec
 
 
 comments_dir = 'res/comments/'
@@ -12,8 +15,8 @@ lmb_training_dir = 'res/language-modeling-benchmark/training/'
 
 
 def main():
-    # make_training_file()
-    train()
+    make_training_file()
+    # train()
 
 
 def make_training_file():
@@ -38,19 +41,36 @@ def make_training_file():
         in os.listdir(lmb_training_dir)
     ]
 
-    with open('res/text', 'w') as training_file:
+    with open('res/sentences.txt', 'w') as training_file:
         for comment_file_name in comment_files:
             with open(comment_file_name, 'r') as comment_file:
                 for line in comment_file:
-                    sentences = comment_to_sentences(line)
-                    training_file.writelines(sentences)
+                    # minimize callstack by calling .lower() now
+                    sentences = comment_to_sentences(line.lower())
+                    formatted_sentences = [
+                        re.sub(' +',' ', ''.join([
+                            c for c in sentence
+                            if str.isalnum(c) or c == ' ']
+                        ) + '\n')
+                        for sentence in sentences
+                    ]
+                    training_file.writelines(formatted_sentences)
                     ### Just for testing:
                     # print '-' * 40 + '\n', line
-                    # print '-' * 10 + '\n', '\n'.join(sentences)
+                    # print '-' * 10 + '\n', '\n'.join(formatted_sentences)
 
         for lmb_file_name in lmb_files:
             with open(lmb_file_name, 'r') as lmb_file:
-                training_file.writelines(lmb_file)
+                for sentence in lmb_file:
+                    formatted_sentence = re.sub(' +',' ', ''.join(
+                        c for c in sentence.lower()
+                        if str.isalnum(c) or c == ' ')
+                    )
+                    training_file.writelines(formatted_sentence)
+                    ### Just for testing:
+                    # print '-' * 40
+                    # print sentence
+                    # print formatted_sentence
 
 
 def comment_to_sentences(comment_line):
@@ -63,24 +83,24 @@ def comment_to_sentences(comment_line):
         comment_text = comment_text[1:-1]
     comment_text = comment_text.replace('""', '"')  # formatting from html
 
-    sentences = tokenize.sent_tokenize(comment_text)
+    sentences = sent_tokenize(comment_text)
     sentences = [sentence + '\n' for sentence in sentences]
 
     return sentences
 
 
 def train():
-    with open('res/text', 'r') as train_file:
-        choo_choo_train = (
-            ''.join(filter(str.isalnum, sentence)).lower().split(' ')
-            for sentence
-            in train_file
-        )
-        model = word2vec.Word2Vec(
-            sentences=iter(choo_choo_train),
-            min_count=10,
-        )
-    model.save('text-no-phrases.bin')
+    # with open('res/sentences.txt', 'r') as train_file:
+    choo_choo_train = word2vec.LineSentence('res/sentences.txt')
+    bigram = phrases.Phrases(sentences=choo_choo_train)
+    trigram = phrases.Phrases(sentences=bigram[choo_choo_train])
+    model = word2vec.Word2Vec(
+        sentences=trigram[choo_choo_train],
+        min_count=15,
+        workers=4,
+    )
+    model.init_sims(replace=True)
+    model.save('reddit-w2v.bin')
 
 
 if __name__ == '__main__':
